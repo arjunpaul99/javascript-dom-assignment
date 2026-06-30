28a3703faa8198a94113cc9e507686540b13c6de
Arjun Paul <anantopaul603@gmail.com>
2026-06-30 21:32:10 +0600

    Project 8 is Recomplet

--- project-08-Gradient Background Generator/script.js
+++ project-08-Gradient Background Generator/script.js
@@ -1,20 +1,33 @@
-const btn = document.querySelector("#btn");
-
+const btn = document.querySelector("#generateBtn");
+const cssCode = document.querySelector("#cssCode");
 
 function randomColor(){
 
     const letters = "0123456789ABCDEF";
+
     let color = "#";
-    for(let i=0;i<6;i++){
-        color += letters[Math.floor(Math.random()*16)];
+
+    for(let i = 0; i < 6; i++){
+
+        color += letters[Math.floor(Math.random() * 16)];
+
     }
+
     return color;
+
 }
 
-btn.addEventListener("click",()=>{
+btn.addEventListener("click", () => {
+
     const color1 = randomColor();
     const color2 = randomColor();
+
     const gradient =
-    `linear-gradient(to right, ${color1}, ${color2})`;
+        `linear-gradient(to right, ${color1}, ${color2})`;
+
     document.body.style.background = gradient;
+
+    cssCode.textContent =
+    `background: ${gradient};`;
+
 });
